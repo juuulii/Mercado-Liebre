8d55b19ffc63f707ea55c9026c2ebcdb4ad3c53d
OscarEMaciel <emaciel19@gmail.com>
2024-10-31 16:27:08 -0300

    contex

--- MercadoLiebre/src/App.jsx
+++ MercadoLiebre/src/App.jsx
@@ -1,36 +1,39 @@
 //react
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 //page
 import LoginPage from "./pages/login.jsx";
 import DashboardPage from "./pages/dahsboard.jsx";
 import AdminPage from "./pages/admin.jsx";
+import { AuthContextProvider } from "./components/Services/AuthContex.jsx";
 
 const App = () => {
-    const router = createBrowserRouter([
-        {
-            element:<LoginPage />,
-            path:"/"
-        },
-        {
-            element:<DashboardPage />, //client
-            path:"/dashboard"
-        },
-        {
-            element:<AdminPage />, //admin
-            path:"/admin"
-        },
-        /*
+  const router = createBrowserRouter([
+    {
+      element: <LoginPage />,
+      path: "/",
+    },
+    {
+      element: <DashboardPage />, //client
+      path: "/dashboard",
+    },
+    {
+      element: <AdminPage />, //admin
+      path: "/admin",
+    },
+    /*
         {
             element:<SuperAdmin />, //client
             path:"/dashboard"
         },
         */
-    ])
-    return(
-        <div className="container">
+  ]);
+  return (
+    <div className="container">
+      <AuthContextProvider>
         <RouterProvider router={router} />
-        </div>
-    )
+      </AuthContextProvider>
+    </div>
+  );
 };
 export default App;
